394284041acb5498f589a16bf17eedfbc93d1bb6
roadrunner-helpcenter <dcaqg1@outlook.com>
2026-07-09 12:56:38 +0530

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -13,3 +13,13 @@ html_js_files = [
     'chatbot.js',
 ]
 html_favicon = '.png'
+conf.py
+
+---
+
+document.addEventListener("DOMContentLoaded", function () {
+    var ze = document.createElement('script');
+    ze.id = 'ze-snippet';
+    ze.src = 'https://static.zdassets.com/ekr/snippet.js?key=7668c976-0fb7-47ee-8740-3e9115bdb10f';
+    document.body.appendChild(ze);
+});
